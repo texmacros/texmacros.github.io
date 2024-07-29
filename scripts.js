50f1197851e96b3db3033bce0bc946673b83c3f4
texmacros <151670914+texmacros@users.noreply.github.com>
2024-07-29 14:02:22 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const animatedBg = document.createElement('div');
+    animatedBg.className = 'animated-bg';
+    document.body.appendChild(animatedBg);
+});
 
